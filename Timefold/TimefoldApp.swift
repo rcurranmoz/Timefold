2f30b47408d50752973a512e724160c2ce6b050b
Ryan Curran <rcurran@mozilla.com>
2026-01-20 08:57:23 -0500

    Initial Commit

---
+++ Timefold/TimefoldApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimefoldApp.swift
+//  Timefold
+//
+//  Created by Ryan Curran on 1/20/26.
+//
+
+import SwiftUI
+
+@main
+struct TimefoldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
